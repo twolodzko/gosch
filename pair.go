4d7cbe38cc1fff0b1b07f1ed5af51d0fe9811f62
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-01-16 18:53:46 +0100

    Pair.IsNull method

--- pair.go
+++ pair.go
@@ -7,22 +7,26 @@ type Pair struct {
 	Next *Pair
 }
 
-func (l *Pair) HasNext() bool {
-	return l.Next != nil
+func (p Pair) HasNext() bool {
+	return p.Next != nil
 }
 
-func (a Pair) String() string {
-	if a == (Pair{}) {
+func (p Pair) IsNull() bool {
+	return p == (Pair{})
+}
+
+func (p Pair) String() string {
+	if p.IsNull() {
 		return "()"
 	}
 
 	var elems string
 	for {
-		elems += fmt.Sprintf("%v", a.This)
-		if a.Next == nil {
+		elems += fmt.Sprintf("%v", p.This)
+		if p.Next == nil {
 			break
 		}
-		a = *a.Next
+		p = *p.Next
 		elems += " "
 	}
 	return fmt.Sprintf("(%s)", elems)

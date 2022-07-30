5d536515073be4a5f306a1357bca3a0c85d123ef
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-07-30 14:40:53 +0200

    Lambda calculus unit tests for eval

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -41,6 +41,72 @@ func Test_ParseEvalPrint(t *testing.T) {
 		{"((lambda () 42))", "42"},
 		{"(lambda (x) x)", "(lambda (x) x)"},
 		{"((lambda (x) x) 3)", "3"},
+		// lambda calculus
+		// true
+		{"((lambda (x y) x) #t #f)", "#t"},
+		// negation of true
+		{"((lambda (x) (x #f #t)) (lambda (x y) x))", "#f"},
+		// negation of false
+		{"((lambda (x) (x #f #t)) (lambda (x y) y))", "#t"},
+		// true and true
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) x) (lambda (x y) x)) #t #f)", "#t"},
+		// true and false
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) x) (lambda (x y) y)) #t #f)", "#f"},
+		// false and true
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) y) (lambda (x y) x)) #t #f)", "#f"},
+	}
+
+	for _, tt := range testCases {
+		parser := parser.NewParser(tt.input)
+		sexprs, err := parser.Read()
+		if err != nil {
+			t.Errorf("for %v got an unexpected error: %v", tt.input, err)
+		}
+
+		for _, sexpr := range sexprs {
+			env := envir.NewEnv()
+			result, err := Eval(sexpr, env)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+			if !cmp.Equal(fmt.Sprintf("%v", result), tt.expected) {
+				t.Errorf("for %v expected %v, got %v", tt.input, tt.expected, result)
+			}
+		}
+	}
+}
+
+func Test_LambdaCalculus(t *testing.T) {
+	// See: https://brilliant.org/wiki/lambda-calculus/
+
+	var testCases = []struct {
+		input    string
+		expected string
+	}{
+		// true
+		{"((lambda (x y) x) #t #f)", "#t"},
+		// false
+		{"((lambda (x y) y) #t #f)", "#f"},
+		// negation of true
+		{"((lambda (x) (x #f #t)) (lambda (x y) x))", "#f"},
+		// negation of false
+		{"((lambda (x) (x #f #t)) (lambda (x y) y))", "#t"},
+		// true and true
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) x) (lambda (x y) x)) #t #f)", "#t"},
+		// true and false
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) x) (lambda (x y) y)) #t #f)", "#f"},
+		// false and true
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) y) (lambda (x y) x)) #t #f)", "#f"},
+		// false and false
+		{"(((lambda (x y) (x y (lambda (x y) y))) (lambda (x y) y) (lambda (x y) y)) #t #f)", "#f"},
+		// true or true
+		{"(((lambda (x y) (x (lambda (x y) x) y)) (lambda (x y) x) (lambda (x y) x)) #t #f)", "#t"},
+		// true or false
+		{"(((lambda (x y) (x (lambda (x y) x) y)) (lambda (x y) x) (lambda (x y) y)) #t #f)", "#t"},
+		// false or true
+		{"(((lambda (x y) (x (lambda (x y) x) y)) (lambda (x y) y) (lambda (x y) x)) #t #f)", "#t"},
+		// false or false
+		{"(((lambda (x y) (x (lambda (x y) x) y)) (lambda (x y) y) (lambda (x y) y)) #t #f)", "#f"},
 	}
 
 	for _, tt := range testCases {

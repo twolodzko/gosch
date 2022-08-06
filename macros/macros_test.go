c6fedfc70ae179e62eca5c5c9402bbe33531b79e
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-08-06 10:15:47 +0200

    Negative test

--- macros/macros_test.go
+++ macros/macros_test.go
@@ -272,6 +272,24 @@ func Test_MatchPattern(t *testing.T) {
 			true,
 			Mapping{"x": "a", "y": "d", "z": "e", "...": types.PairFromArray([]types.Sexpr{"b", "c"})},
 		},
+		{
+			// (x ((() (...) y) z)
+			PairPattern{[]Pattern{
+				IdentifierPattern{"x"},
+				PairPattern{[]Pattern{
+					PairPattern{},
+					PairPattern{[]Pattern{EllipsisPattern{}}},
+					IdentifierPattern{"y"}}},
+				IdentifierPattern{"z"}}},
+			types.PairFromArray([]types.Sexpr{
+				types.Symbol("a"),
+				types.PairFromArray([]types.Sexpr{
+					&types.Pair{},
+					types.Symbol("d")}),
+				types.Symbol("e")}),
+			false,
+			Mapping{},
+		},
 	}
 
 	for _, tt := range testCases {
